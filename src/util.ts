fe4e34f017f2c80f00d37d454039a75a0fcf8e85
jgw96 <jgw9617@gmail.com>
2018-05-02 13:50:13 -0500

    fix(): injectGlobalPath is now correct on windows

--- src/util.ts
+++ src/util.ts
@@ -34,7 +34,7 @@ export function getRenderOptions(opts: d.PluginOptions, sourceText: string, file
     const injectText = injectGlobalPaths.map(injectGlobalPath => {
       if (!path.isAbsolute(injectGlobalPath)) {
         // convert any relative paths to absolute paths relative to the project root
-        injectGlobalPath = path.join(context.config.rootDir, injectGlobalPath);
+        injectGlobalPath = normalizePath(path.join(context.config.rootDir, injectGlobalPath));
       }
 
       return `@import "${injectGlobalPath}";`;
@@ -59,3 +59,39 @@ export function createResultsId(fileName: string) {
   pathParts[pathParts.length - 1] = 'css';
   return pathParts.join('.');
 }
+
+export function normalizePath(str: string) {
+  // Convert Windows backslash paths to slash paths: foo\\bar ➔ foo/bar
+  // https://github.com/sindresorhus/slash MIT
+  // By Sindre Sorhus
+  if (typeof str !== 'string') {
+    throw new Error(`invalid path to normalize`);
+  }
+  str = str.trim();
+
+  if (EXTENDED_PATH_REGEX.test(str) || NON_ASCII_REGEX.test(str)) {
+    return str;
+  }
+
+  str = str.replace(SLASH_REGEX, '/');
+
+  // always remove the trailing /
+  // this makes our file cache look ups consistent
+  if (str.charAt(str.length - 1) === '/') {
+    const colonIndex = str.indexOf(':');
+    if (colonIndex > -1) {
+      if (colonIndex < str.length - 2) {
+        str = str.substring(0, str.length - 1);
+      }
+
+    } else if (str.length > 1) {
+      str = str.substring(0, str.length - 1);
+    }
+  }
+
+  return str;
+}
+
+const EXTENDED_PATH_REGEX = /^\\\\\?\\/;
+const NON_ASCII_REGEX = /[^\x00-\x80]+/;
+const SLASH_REGEX = /\\/g;
