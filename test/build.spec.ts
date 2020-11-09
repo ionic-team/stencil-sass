387ebcbb79b2b3d97b9d20e78925c432580ba546
Daniel Costrasel <gleesik.dark@gmail.com>
2020-11-09 22:06:15 +0200

    Created sass fixtures and updated spec

--- test/build.spec.ts
+++ test/build.spec.ts
@@ -28,7 +28,7 @@ describe('test build', () => {
     };
   })
 
-  it('transform', async () => {
+  it('transform scss', async () => {
     const filePath = path.join(__dirname, 'fixtures', 'scss', 'test-a.scss');
     const sourceText = fs.readFileSync(filePath, 'utf8');
     const s = sass();
@@ -39,7 +39,7 @@ describe('test build', () => {
     expect(results.diagnostics).toEqual(undefined);
   });
 
-  it('transform, import', async () => {
+  it('transform, import scss', async () => {
     const filePath = path.join(__dirname, 'fixtures', 'scss', 'test-b.scss');
     const sourceText = fs.readFileSync(filePath, 'utf8');
     const s = sass();
@@ -52,7 +52,7 @@ describe('test build', () => {
     expect(results.diagnostics).toEqual(undefined);
   });
 
-  it('transform, error', async () => {
+  it('transform, error scss', async () => {
     const filePath = path.join(__dirname, 'fixtures', 'scss', 'test-c.scss');
     const sourceText = fs.readFileSync(filePath, 'utf8');
     const s = sass();
@@ -82,6 +82,62 @@ describe('test build', () => {
     expect(context.diagnostics[0].lines[2].errorCharStart).toEqual(-1);
     expect(context.diagnostics[0].lines[2].errorLength).toEqual(-1);
     expect(context.diagnostics[0].lines[2].text).toEqual('  div{color:green}');
+  });
+
+  it('transform sass', async () => {
+    const filePath = path.join(__dirname, 'fixtures', 'sass', 'test-a.sass');
+    const sourceText = fs.readFileSync(filePath, 'utf8');
+    const s = sass();
+
+    const results = await s.transform(sourceText, filePath, context) as any;
+    expect(results.code).toContain('color: red');
+    expect(results.dependencies).toEqual([]);
+    expect(results.diagnostics).toEqual(undefined);
+  });
+
+  it('transform, import sass', async () => {
+    const filePath = path.join(__dirname, 'fixtures', 'sass', 'test-b.sass');
+    const sourceText = fs.readFileSync(filePath, 'utf8');
+    const s = sass();
+
+    const results = await s.transform(sourceText, filePath, context) as any;
+    expect(results.code).toContain('color: red');
+    expect(results.dependencies).toEqual([
+      path.join(__dirname, 'fixtures', 'sass', 'variables.sass')
+    ]);
+    expect(results.diagnostics).toEqual(undefined);
+  });
+
+  it('transform, error sass', async () => {
+    const filePath = path.join(__dirname, 'fixtures', 'sass', 'test-c.sass');
+    const sourceText = fs.readFileSync(filePath, 'utf8');
+    const s = sass();
+
+    await s.transform(sourceText, filePath, context);
+    expect(context.diagnostics).toHaveLength(1);
+    expect(context.diagnostics[0].level).toEqual('error');
+    expect(context.diagnostics[0].language).toEqual('sass');
+    expect(context.diagnostics[0].lineNumber).toEqual(3);
+    expect(context.diagnostics[0].columnNumber).toEqual(20);
+    expect(context.diagnostics[0].lines.length).toEqual(3);
+
+    expect(context.diagnostics[0].lines[0].lineIndex).toEqual(1);
+    expect(context.diagnostics[0].lines[0].lineNumber).toEqual(2);
+    expect(context.diagnostics[0].lines[0].errorCharStart).toEqual(-1);
+    expect(context.diagnostics[0].lines[0].errorLength).toEqual(-1);
+    expect(context.diagnostics[0].lines[0].text).toEqual('  color: blue');
+
+    expect(context.diagnostics[0].lines[1].lineIndex).toEqual(2);
+    expect(context.diagnostics[0].lines[1].lineNumber).toEqual(3);
+    expect(context.diagnostics[0].lines[1].errorCharStart).toEqual(19);
+    expect(context.diagnostics[0].lines[1].errorLength).toEqual(1);
+    expect(context.diagnostics[0].lines[1].text).toEqual('hello i am an error!');
+
+    expect(context.diagnostics[0].lines[2].lineIndex).toEqual(3);
+    expect(context.diagnostics[0].lines[2].lineNumber).toEqual(4);
+    expect(context.diagnostics[0].lines[2].errorCharStart).toEqual(-1);
+    expect(context.diagnostics[0].lines[2].errorLength).toEqual(-1);
+    expect(context.diagnostics[0].lines[2].text).toEqual('div');
   });
 
   it('name', async () => {
