d4b748c58c4d29d484f1048aa5818655eb7de396
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-12-01 16:56:32 -0500

    fix __dirname

--- rollup.plugin.sass.mjs
+++ rollup.plugin.sass.mjs
@@ -1,8 +1,10 @@
+import { fileURLToPath } from 'node:url'
 import * as path from 'path';
 import { minify } from 'terser';
 
 export default function() {
-  const sassFilePath = path.join(__dirname, 'node_modules', 'sass', 'sass.dart.js');
+  const sassExecutable = path.join('node_modules', 'sass', 'sass.dart.js')
+  const sassFilePath = fileURLToPath(new URL('node_modules/sass/sass.dart.js', import.meta.url));
   return {
     resolveId(id) {
       if (id === 'sass') {
