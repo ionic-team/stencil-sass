c7cac9d1588769e885b4255cfd99a61b82a92385
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2022-12-01 16:46:46 -0500

    chore(config): use new way of importing package.json

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,9 @@
-import pkg from './package.json';
+import { createRequire } from 'node:module';
 import pluginSass from './rollup.plugin.sass.js';
 import rollupResolve from '@rollup/plugin-node-resolve';
 
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
 
 export default {
   input: 'dist/index.js',
